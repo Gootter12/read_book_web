d786baaf17f9230df7a0830650592fb37b1208fc
gootter <serenity.lh.h@gmail.com>
2019-07-19 18:09:30 +0800

    second

---
+++ components/bookitem/src/bookitem.js
@@ -0,0 +1,24 @@
+new Vue({
+  el: '#gbs',
+  data() {
+    return {
+      info: null
+    }
+  },
+  mounted() {
+    // axios.post('https://www.read-a-book.wang/get_bookshelf',{"sessionId":6})
+    //   .then(response => (this.info = response))
+    //   .catch(error => (this.info = error));
+    axios({
+      url: 'https://www.read-a-book.wang/get_bookshelf',
+      method: 'POST',
+      //发送格式为json
+      data: JSON.stringify({"sessionId":6}),
+      headers:
+        {
+          'Content-Type': 'text/plain;charset=utf-8'
+        }
+    }).then(response => {this.info = response} )
+
+  }
+})
